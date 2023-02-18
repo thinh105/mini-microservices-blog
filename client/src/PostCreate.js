06d477b2c8552f5fa7f6967eedd4dab6773fdd25
Thinh <maxiqboy@gmail.com>
2023-02-18 19:09:46 +0700

    final code

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://posts-clusterip-srv:4000/posts", {
+    await axios.post("http://posts.com/posts/create", {
       title,
     });
 
